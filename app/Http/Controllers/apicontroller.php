292d4c5be53565e30dbfb6cff4f074fd81969371
mashhisyam <haryoh48@gmail.com>
2021-04-14 21:38:52 +0700

    Membuat Api controller untuk RESTful API

--- app/Http/Controllers/apicontroller.php
+++ app/Http/Controllers/apicontroller.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\ApiModel;
 use Illuminate\Http\Request;
 
@@ -9,5 +10,20 @@ class apicontroller extends Controller
     public function get_all_users()
     {
         return response()->json(ApiModel::all(), 200);
+    }
+    public function insert_data_users(Request $request)
+    {
+        $insert_users = new ApiModel;
+        $insert_users->fullname = $request->fullname;
+        $insert_users->phonenumber = $request->phonenumber;
+        $insert_users->address = $request->address;
+        $insert_users->email = $request->email;
+        $insert_users->password = $request->password;
+        $insert_users->save();
+        return response([
+            'status'=>'OK',
+            'message'=>'Users Disimpan',
+            'status'=>$insert_users
+        ],200);
     }
 }
