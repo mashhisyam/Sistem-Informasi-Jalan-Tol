94a5d82e080f48bd3b48271f2bd43a5887be3394
mashhisyam <haryoh48@gmail.com>
2021-04-19 16:17:38 +0700

    Membuat RESTful API

--- app/Http/Controllers/apicontroller.php
+++ app/Http/Controllers/apicontroller.php
@@ -2,18 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\ApiModel;
+use App\Models\UsersModel;
 use Illuminate\Http\Request;
 
 class apicontroller extends Controller
 {
     public function get_all_users()
     {
-        return response()->json(ApiModel::all(), 200);
+        return response()->json(UsersModel::all(), 200);
     }
+
     public function insert_data_users(Request $request)
     {
-        $insert_users = new ApiModel;
+        $insert_users = new UsersModel;
         $insert_users->fullname = $request->fullname;
         $insert_users->phonenumber = $request->phonenumber;
         $insert_users->address = $request->address;
@@ -21,9 +22,20 @@ class apicontroller extends Controller
         $insert_users->password = $request->password;
         $insert_users->save();
         return response([
-            'status'=>'OK',
-            'message'=>'Users Disimpan',
-            'status'=>$insert_users
-        ],200);
+            'status' => 'OK',
+            'message' => 'Users Disimpan',
+            'status' => $insert_users
+        ], 200);
+    }
+
+    public function update_data_users(Request $request, $id)
+    {
+        $check = UsersModel::firstWhere('users_id', $id);
+        if ($check) {
+            echo 'data yang anda cari tersedia';
+            $data_users = UsersModel::find($id);
+        } else {
+            echo 'tidak ada';
+        }
     }
 }
